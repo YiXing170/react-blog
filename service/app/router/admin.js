b9c6d2e0ced4dd3b49d8d475c54a6a950aca53e2
yijiliang <857719943@qq.com>
2020-11-20 23:12:10 +0800

    add session

--- service/app/router/admin.js
+++ service/app/router/admin.js
@@ -1,5 +1,6 @@
 module.exports = app => {
   const { router, controller } = app
+  var adminauth = app.middleware.adminauth()
   router.get('/admin/index', adminauth, controller.admin.main.index)
   router.post('/admin/checkOpenId', controller.admin.main.checkLogin)
 }
